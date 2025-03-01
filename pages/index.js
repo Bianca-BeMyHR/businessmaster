47441a6aaad15848750af87adb0f3945ed49ada7
Bianca-BeMyHR <bianca@bemyhr.ca>
2025-02-28 17:55:06 -0700

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,9 @@ import Link from 'next/link';
 export default function Home() {
   return (
 <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 text-gray-900">
-<h1 className="text-3xl font-bold">Welcome to Financial Tracker</h1>
+<h1 className="text-3xl font-bold">Welcome to Business Master</h1>
 <p className="mt-2 text-lg">
-  Manage your personal and business finances
+  Bringing your ideas to life using tech
   </p>
   <div className="mt-6 space-x-4">
     <Link href="/login">
