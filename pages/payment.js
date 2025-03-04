8a138c229f60dad152ede006b7debc044143277c
Bianca-BeMyHR <bianca@bemyhr.ca>
2025-03-03 22:51:01 -0700

    Update payment.js

--- pages/payment.js
+++ pages/payment.js
@@ -16,7 +16,7 @@ export default function PaymentPage() {
           <li>If you face any issues, please contact our support team.</li>
         </ul>
 
-        <Link href="https://www.helcim.com/payments/" className="w-full p-4 bg-blue-500 hover:bg-blue-600 text-white rounded-lg text-center">
+        <Link href="https://bemyhr.myhelcim.com/hosted/?token=3d3f3a95ee4e9920a7f032&amount=400.00&amountHash=9c2fc73bdcb5b0ce9e2273bb82708f032f69df9093fae520cdc15c0e02df3bfa" className="w-full p-4 bg-blue-500 hover:bg-blue-600 text-white rounded-lg text-center">
           Proceed to Payment
         </Link>
       </div>
